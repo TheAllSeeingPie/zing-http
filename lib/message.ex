56385ed2b9934ca314cca96a2e851302cbc9e155
Sky iD [LEEML0231806] <skyid.LEEML0231806@id.bskyb.com>
2016-09-30 10:25:10 +0100

    Updated processing and tests

--- lib/message.ex
+++ lib/message.ex
@@ -1,11 +1,14 @@
 defmodule Message do
+	import List, only: :functions
+	import Regex, only: :functions
+
 	defstruct verb: "", path: "", headers: {}, payload: ""
 
 	def parse(message) do
-		[verb, path, rawheaders, payload] = Regex.run ~r/^([A-Z]+) (.+) HTTP\/1.[01]\r{0,1}\n((?:[A-z]+:.+\r{0,1}\n)+)(?:\r{0,1}\n){0,1}([\s\S]+)/, message, capture: :all_but_first
-		headers = for header <- Regex.scan(~r/([A-z]+): (.+)/, rawheaders, capture: :all_but_first),
+		[verb, path, rawheaders, payload] = run ~r/^([A-Z]+) (.+) HTTP\/1.[01]\r{0,1}\n((?:[A-z]+:.+\r{0,1}\n)+)(?:\r{0,1}\n){0,1}([\s\S]+)/, message, capture: :all_but_first
+		headers = for header <- scan(~r/([A-z]+): (.+)/, rawheaders, capture: :all_but_first),
 			into: Map.new,
-			do: { String.to_atom(hd header), hd tl header }
+			do: { String.to_atom(first header), last header }
 		%Message{verb: verb, path: path, headers: headers, payload: payload}
 	end
 end
